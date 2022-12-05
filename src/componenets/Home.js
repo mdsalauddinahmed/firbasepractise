b8530fe8edd27b66d87339ddeb91896eb80bcdf0
salauddinAmir <mdsalauddinahmed.amir@gmail.com>
2022-12-05 11:56:27 +0600

    added conditionals

--- src/componenets/Home.js
+++ src/componenets/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import {getAuth,GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import app from '../firebaseConfig';
+const auth =getAuth(app);
 
 const Home = () => {
+    const provider = new GoogleAuthProvider();
+    const handleSignIn=()=>{
+            signInWithPopup(auth,provider)
+            .then(result =>{
+              const user = result.user;
+              console.log(user);
+
+            })
+            .catch(error =>{
+              console.log('erroe:',error)
+            })
+    }
     return (
         <div>
-            <h1>Home</h1>
+          <button onClick={handleSignIn}>Send me</button>
             
         </div>
     );
--- src/componenets/SignIn.js
+++ src/componenets/SignIn.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+ 
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import app from '../firebaseConfig'
+
+const auth = getAuth(app);
 
 const SignIn = () => {
+    const[user,setUser]= useState('');
+    const provider = new  GoogleAuthProvider();
+    const handleSignIn =()=>{
+       
+        signInWithPopup(auth,provider)
+        .then (result => {
+            // eslint-disable-next-line no-use-before-define
+           
+            const user =result.user;
+            setUser(user);
+            console.log(user)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+    }
+
+    const handleSignOut =()=>{
+        signOut(auth)
+        .then(()=>{
+           setUser('')
+        })
+         .catch(()=>{
+                setUser('')
+         })
+    }
     return (
          
-             <div>hello</div>
+             <div>
+                {user.email ? <button onClick={handleSignOut}>Google sign out</button> :
+                 <button onClick={handleSignIn}>Google sign in</button>
+               }
+                { user.email && <div>
+                     User name: {user.displayName}
+                     <br/>
+                     User Email : {user.email}
+                     <br/>
+                     <img src={user.photoURL} alt="" />
+                 </div>}
+             </div>
     );
 };
 
